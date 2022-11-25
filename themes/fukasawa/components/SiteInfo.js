bf3a99cd3491780862b8cff8f6c30d77efb1b82b
JingXiang <jkfeidao@gmail.com>
2022-11-25 16:34:56 +0800

    Update SiteInfo.js

--- themes/fukasawa/components/SiteInfo.js
+++ themes/fukasawa/components/SiteInfo.js
@@ -21,6 +21,7 @@ function SiteInfo ({ title }) {
             <span className='hidden busuanzi_container_site_pv'> <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
             <span className='pl-2 hidden busuanzi_container_site_uv'> <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
             <br />
+            </span>
             <h1>{title}</h1>
         </footer>
   )
